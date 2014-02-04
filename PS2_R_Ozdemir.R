44c4eb1ce8281f1edecec75c5f0bed0f2634474a
eozdemir <eozdemir@wustl.edu>
2014-02-04 16:22:54 -0600

    questions1&2
    worked on the function

--- PS2_R_Ozdemir.R
+++ PS2_R_Ozdemir.R
@@ -1,47 +1,44 @@
 rm(list = ls()) #clear the workspace
-results<- numeric(5)
+results<- sample(1:100, 10, replace=TRUE)
 m.fnc<- function(results){
   i<- as.numeric(substr(results, start=1, stop=1))
   for(n in 1:9){
   X<- (sum(results[i==n]))/(sum(results)-log(1+1/i))
-  m<- max(X)
+  m<- sqrt(length(results))*max(X)
   }
   if(m>=0.851 & m<0.967){
     print("Fraud at 0.10 significance level")
   }
-  if(m>=0.967 & m<1.212){
+  else if(m>=0.967 & m<1.212){
     print("Fraud at 0.05 significance level")
   }
-  if(m>=1.212){
+  else if(m>=1.212){
     print("Fraud at 0.01 significance level")
   }
-  if(m<0.851){
+  else{
     print("No fraud")
   }
 }
-trial<- c(10,20,25,30,40)
-m.fnc(trial)
+
 
 rm(list = ls()) #clear the workspace
-results<- numeric(5)
+results<- sample(1:100, 10, replace=TRUE)
 d.fnc<- function(results){
   i<- as.numeric(substr(results, start=1, stop=1))
   for(n in 1:9){
     Y<- sum((sum(results[i==n]))/(sum(results)-log(1+1/i))^2)
-    d<- sqrt(Y)
+    d<- sqrt(length(results))*sqrt(Y)
     }
   if(d>=1.212 & d<1.330){
     print("Fraud at 0.10 significance level")
   }
-  if(d>=1.330 & d<1.569){
+  else if(d>=1.330 & d<1.569){
     print("Fraud at 0.05 significance level")
   }
-  if(d>=1.569){
+  else if(d>=1.569){
     print("Fraud at 0.01 significance level")
   }
-  if(d<1.212){
+  else{
     print("No fraud")
   }
 }
-trial<- c(10,20,25,30,40)
-d.fnc(trial)
