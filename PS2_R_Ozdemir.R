26cadcda26bcdacc9b099922b859c5d4d45aeb0e
eozdemir <eozdemir@wustl.edu>
2014-02-04 18:01:01 -0600

    questions1&2
    worked on second question
    but R doesn't respond any more

--- PS2_R_Ozdemir.R
+++ PS2_R_Ozdemir.R
@@ -1,16 +1,17 @@
 rm(list = ls()) #clear the workspace
 ##Question 1
+set.seed(12345)
 results<- sample(1:100, 10, replace=TRUE) #generate random election results 
 my.fnc<- function(results,stat){ #function to derive the statistics
   i<- as.numeric(substr(results, start=1, stop=1)) #takes the first integers and stores
-  if(stat="m"){        #if stat option is specified as "m", returns m statistics
+  if(stat=="m"){        #if stat option is specified as "m", returns m statistics
     for(n in 1:9){     #applies the formula for m statistics for every integer stored
       X<- (sum(results[i==n]))/(sum(results)-log(1+1/i))
       m<- sqrt(length(results))*max(X)
       return(m)
     }
   }  
-  else if(stat="d"){    #if stat option is specified as "d", returns d statistics
+  else if(stat=="d"){    #if stat option is specified as "d", returns d statistics
     for(n in 1:9){      #applies the formula for d statistics for every integer stored
       Y<- sum((sum(results[i==n]))/(sum(results)-log(1+1/i))^2)
       d<- sqrt(length(results))*sqrt(Y)
@@ -29,32 +30,28 @@ my.fnc<- function(results,stat){ #function to derive the statistics
     output<-list(m,d)  #return two values in a function
     return(output)
 }
-  
+ m
 ##Question 2
-#Print fraud information
-if(m>=0.851 & m<0.967){
-  print("Fraud at 0.10 significance level")
-}
-else if(m>=0.967 & m<1.212){
-  print("Fraud at 0.05 significance level")
-}
-else if(m>=1.212){
-  print("Fraud at 0.01 significance level")
-}
-else{
-  print("No fraud")
-}
-
-#Print fraud information
-if(d>=1.212 & d<1.330){
-  print("Fraud at 0.10 significance level")
-}
-else if(d>=1.330 & d<1.569){
-  print("Fraud at 0.05 significance level")
-}
-else if(d>=1.569){
-  print("Fraud at 0.01 significance level")
-}
-else{
-  print("No fraud")
+print.benfords<- function(){
+  if(m>=0.851 & m<0.967){
+    asterisk.m<- c("*")
+    }else if(m>=0.967 & m<1.212){
+    asterisk.m<- c("**")
+    }else if(m>=1.212){
+    asterisk.m<- c("***")
+    }else{
+    asterisk.m<- c("")
+    }
+  
+  if(d>=1.212 & d<1.330){
+    asterisk.d<- c("*")
+    }else if(d>=1.330 & d<1.569){
+    asterisk.d<- c("**")
+    }else if(d>=1.569){
+    asterisk.d<- c("***")
+    }else{
+    asterisk.d<- c("")
+  }
+  output.list<- list(m=c(m, asterisk.m) d=c(d, asterisk.d))
+  table(output.list)
 }
